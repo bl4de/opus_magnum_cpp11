9155b2d3a83b0cd996b0d79a3ad3c2962999998b
bloorq@gmail.com <bloorq@gmail.com>
2018-01-04 01:46:25 +0000

    04.01.2018 - 3.5 Inicjalizacja

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // definiowanie typow
+    int x = 10;
+    int y(20);
+    int z {30};
+    int v = {40};
+
+    cout<<x<<endl
+    <<y<<endl
+    <<z<<endl
+    <<v<<endl;
+}
