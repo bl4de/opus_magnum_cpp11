ff28adb8f0c8ada352e2c411e7ecc837ac322aa5
bloorq@gmail.com <bloorq@gmail.com>
2018-01-07 19:37:53 +0000

    constexpr

---
+++ 6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    constexpr double pi = 3.1415926;
+    constexpr double objkuli = 4.00 / 3.00 * pi;
+
+    const int rozm = 10;
+    int tab[rozm];
+    
+}
