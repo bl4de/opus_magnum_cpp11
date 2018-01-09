2edb5c28467d6bc041b8ef8a9578cd284bcff543
bloorq@gmail.com <bloorq@gmail.com>
2018-01-09 01:22:39 +0000

    typedef, using

---
+++ 7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    typedef double vars;
+    using wsp = int;
+    vars x =10.5;
+    vars y = 12.0;
+    wsp z = 20;
+
+    cout<<(x*y)/z;
+
+    
+}
