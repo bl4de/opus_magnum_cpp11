398612764bb63c48383c8a3bb654139b9ac208ca
bloorq@gmail.com <bloorq@gmail.com>
2018-01-09 01:26:53 +0000

    typedef, using

--- 7.cpp
+++ 7.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int main() {
     typedef double vars;
+    using something = int;
+
+    constexpr something dwiedwojki = 22;
+    volatile something jestniema = 32;
     using wsp = int;
+    
     vars x =10.5;
     vars y = 12.0;
     wsp z = 20;
 
-    cout<<(x*y)/z;
-
-    
+    cout<<(((x*y)/z) * dwiedwojki)/jestniema
+    <<endl;
 }
