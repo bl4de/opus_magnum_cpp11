db1444d033ebce338bf8856478b336c65156b704
bloorq@gmail.com <bloorq@gmail.com>
2018-01-16 01:05:13 +0000

    operatory ++/--

---
+++ 11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 5, b = 5, c = 5, d = 5;
+  cout << a++ << endl;
+  cout << ++b << endl;
+  cout << c-- << endl;
+  cout << --d << endl;
+
+  cout << "\n\n";
+
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+}
