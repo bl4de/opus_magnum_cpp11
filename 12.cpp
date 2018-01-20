8abab61187e3a23b938c80344d8b8cf018b846eb
bloorq@gmail.com <bloorq@gmail.com>
2018-01-20 23:55:13 +0000

    sizeof

--- 12.cpp
+++ 12.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
- 
+  int a = 10;
+  int b = 20;
+
+  if (a or b) {
+    cout << "a lub b" << endl;
+  }
+
+  if (a and b) {
+    cout << "a i b" << endl;
+  }
+
+  cout<<sizeof(size_t);
 }
