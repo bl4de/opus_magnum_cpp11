c12bf1aaa74744ff0afb929a8a3f2951d4467d91
bloorq@gmail.com <bloorq@gmail.com>
2018-01-24 00:53:51 +0000

    casting operators

---
+++ 14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // static_cast
+    double pi = 3.1415926;
+    cout<<"int pi = "<<static_cast<int>(pi)<<endl;
+
+    int nah = 10;
+    cout<<"double nah: "<<static_cast<double>(nah)<<endl;
+
+    // const_cast
+    const int var = 100;
+    int* varMemAddr;
+    // error: assigning to 'int *' from incompatible type 'const int *'
+    // varMemAddr = &var;  
+
+    // ???
+    // const_cast<int>(varMemAddr) = &var;
+}
