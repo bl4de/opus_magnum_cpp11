cb0a7e1df89bbfa85ef169378a2fcf70af10a842
bloorq@gmail.com <bloorq@gmail.com>
2018-01-29 00:43:19 +0000

    vectors [1]

---
+++ vectors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<string> fitgurls = {"Natalia Gacka", "Sylwia Szostak",
+                             "Kasia Dziurska"};
+  string tmp;
+  cout << "\n" + fitgurls[0];
+
+  tmp = fitgurls[0];
+  fitgurls[0] = fitgurls[2];
+  fitgurls[2] = tmp;
+
+  fitgurls.push_back("Lauren Drain");
+
+  for (int i = 0; i < fitgurls.size(); i++) {
+    cout << "\nFitGurl " << i << " : " << fitgurls[i];
+  }
+  cout << endl;
+
+  exit(0);
+}
