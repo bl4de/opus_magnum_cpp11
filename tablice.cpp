02e370dcd63781c5826c1cb1c246563732c76cf0
bloorq@gmail.com <bloorq@gmail.com>
2018-02-18 00:33:28 +0000

    tablice [1]

---
+++ tablice.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#include "oposy.cpp"
+
+using namespace std;
+using namespace oposy;
+
+void podw(int tab[]) {
+    for (int i = 0; i < 5; i++) {
+        tab[i] *= tab[i];
+    }
+}
+
+int main() {
+    int tab[5]{1,2,3,4,5};
+
+    podw(tab);
+
+    for (int i = 0; i < 5; i++) {
+        valueOf(tab[i]);
+    }
+}
