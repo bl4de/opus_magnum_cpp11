dbd3691c976ae9cb7c0ae507dea7f9e5a264bcaf
bloorq@gmail.com <bloorq@gmail.com>
2018-03-05 21:08:27 +0000

    tabl. wsk do char

--- tabl_wsk.cpp
+++ tabl_wsk.cpp
@@ -6,6 +6,19 @@
 using namespace std;
 using namespace oposy;
 
+void przedz_tab(const char tab[]) {
+  int i = 0;
+  while (tab[i]) {
+    cout << tab[i++] << "-";
+  }
+}
+
+void przedz_wsk(const char *w) {
+  while (*w) {
+    cout << *(w++) << "-";
+  }
+}
+
 int main() {
   const char *stops[]{"Parnell",      "OConnel Upper",      "Westmoreland",
                       "Dawson",       "St. Stephens Green", "Dundrum",
@@ -19,8 +32,11 @@ int main() {
 
   cout << "Green Line LUAS Stops:\n";
   int i = 0;
-  while ((current = stops[i++])) {
-    cout << current << "\n";
+  while ((current = stops[i])) {
+    cout << "\n" << current;
+    przedz_tab(stops[i]);
+    przedz_wsk(stops[i]);
+    i++;
   }
 
   //   for (int i = 0; i < 19; i++) {
