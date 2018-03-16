2d009603fd771592f6694330b8640a288f5eef5b
bloorq@gmail.com <bloorq@gmail.com>
2018-03-16 00:13:35 +0000

    Klasy (3)

--- class01.cpp
+++ class01.cpp
@@ -8,19 +8,31 @@ using namespace oposy;
 
 class Tcar {
     private:
-
+        string manufacturer {"Audi"};
     public:
-        string manufacturer;
+        
         string model;
 
-        string getManufacturer();
+        string getFullName() {
+            return manufacturer + " " + model;
+        }
 };
 
-string Tcar::getManufacturer() {
-    return manufacturer;
-}
+// string Tcar::getFullName() {
+//     return manufacturer + " " + model;
+// }
 
 int main() {
     Tcar a4;
-    valueOf(a4.getManufacturer());
+    a4.model = "A4";
+    valueOf(a4.getFullName());
+    valueOf(sizeof(a4));
+    Tcar rs5;
+    Tcar *myaudi;
+
+    myaudi = &rs5;
+    myaudi->model = "RS 5";
+    string fullname = myaudi->getFullName();
+
+    valueOf(fullname);
 }
