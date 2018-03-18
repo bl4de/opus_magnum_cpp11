8ee5faf71a5ff1640e35aba351e420b3f3d3146e
bloorq@gmail.com <bloorq@gmail.com>
2018-03-18 23:42:16 +0000

    cars - wersja z konstruktorem

--- cars.cpp
+++ cars.cpp
@@ -1,17 +1,15 @@
 #include "Tcar.h"
 #include "oposy.cpp"
 
-void showCar(Tcar &audi) {
-    oposy::valueOf(audi.getDescription());
-}
+void showCar(Tcar &audi) { oposy::valueOf(audi.getDescription()); }
 
 int main() {
-    Tcar audia4("Audi", "A4 1.8 TFSI");
-    showCar(audia4);
+  Tcar audia4{"Audi", "A4 1.8 TFSI"};
+  showCar(audia4);
 
-    Tcar audirs5("Audi", "RS5");
-    Tcar *myaudi;
-    myaudi = &audirs5;
-    
-    showCar(*myaudi);
+  Tcar audirs5{"Audi", "RS5"};
+  Tcar *myaudi;
+  myaudi = &audirs5;
+
+  showCar(*myaudi);
 }
