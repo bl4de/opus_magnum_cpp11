2410d87e46461231479f555829d922f777fc8f84
bloorq@gmail.com <bloorq@gmail.com>
2018-03-19 00:10:57 +0000

    cars - wersja z destruktorem i zakresem lokalnym i wywolaniem prez wskaznik

--- cars.cpp
+++ cars.cpp
@@ -1,29 +1,30 @@
 #include "Tcar.h"
 #include "oposy.cpp"
 
-void showCar(Tcar audi) {
+void showCar(Tcar* audi) {
   static int licznik = 1;
-  oposy::valueOf(audi.getDescription());
-  std::cout << " licznik wysw.: " << licznik++;
+  oposy::valueOf(audi->getDescription());
+  std::cout << " licznik wysw.: " << licznik++ << "\n";
 }
 
 Tcar ada("Audi", "A4 B5 1.8 ADR 125KM");
 
 int main() {
 
-  showCar(ada);
+  showCar(&ada);
 
   {
     Tcar audia4{"Audi", "A4 1.8 TFSI"};
     Tcar ada("Audi", "A4 B5 1.8 ADR 125KM");
-    showCar(audia4);
+    showCar(&audia4);
 
     Tcar audirs5{"Audi", "RS5"};
     Tcar *myaudi;
     myaudi = &audirs5;
 
-    showCar(*myaudi);
-    showCar(::ada);
-    showCar(ada);
+    showCar(myaudi);
+    showCar(&audirs5);
+    showCar(&::ada);
+    showCar(&ada);
   }
 }
