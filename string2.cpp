716855c226fcb1f0bdd758d1a7036601b7981ae7
bloorq@gmail.com <bloorq@gmail.com>
2018-04-15 00:01:54 +0100

    Stringi (1)

--- string2.cpp
+++ string2.cpp
@@ -2,7 +2,15 @@
 #include <string>
 using namespace std;
 
-int main() { 
-    string test = "Pierwszy string"; 
-    string inny = "Jakis inny";
+int main() {
+  string test = "Pierwszy string";
+  string inny = "Jakis inny";
+  cout << test.at(2) << endl;
+
+  test.reserve(20);
+  cout << test.size() << endl;
+
+  test.resize(100);
+  cout << test.size() << endl;
+  cout << test[43] << endl;
 }
