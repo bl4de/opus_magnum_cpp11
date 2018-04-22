f6bdf132e06f3577c5b3e8225a2e20e6b38cc5b9
bloorq@gmail.com <bloorq@gmail.com>
2018-04-23 00:57:21 +0100

    string2 cd.

--- string2.cpp
+++ string2.cpp
@@ -13,4 +13,13 @@ int main() {
   test.resize(100);
   cout << test.size() << endl;
   cout << test[43] << endl;
+
+  string wyj = "Wyjatek";
+  try {
+    for (unsigned int p = 0; p < wyj.length() + 3; p++) {
+      cout << "znak z pozycji " << p << " to : " << wyj.at(p) << endl;
+    }
+  } catch (std::out_of_range) {
+    cout << "pozycja poza stringiem!" << endl;
+  }
 }
