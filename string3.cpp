c972ba07cb16397898ad8f70bab859adcce77fa4
bloorq@gmail.com <bloorq@gmail.com>
2018-04-29 23:24:43 +0100

    substr() + find()

--- string3.cpp
+++ string3.cpp
@@ -9,7 +9,7 @@ using namespace oposy;
 int main() {
   string text = "Let me introduce myself, my name is Korg";
   try {
-    string _name = text.substr(12, 4);
+    string _name = text.substr(text.find("Korg",0), 4);
     cout << "_name = " << _name << endl;
   } catch (std::out_of_range &e) {
     cout << "Exception: " << e.what() << endl;
