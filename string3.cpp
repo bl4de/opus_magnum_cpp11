521cda3ff72ca292596e3914f944df8a43099d38
bloorq@gmail.com <bloorq@gmail.com>
2018-04-30 00:39:52 +0100

    insert()

--- string3.cpp
+++ string3.cpp
@@ -8,18 +8,23 @@ using namespace oposy;
 
 int main() {
   string text = "Let me introduce myself, my name is Korg";
-  
+
   try {
-    string _name = text.substr(text.find("Korg",0), 4);
+    string _name = text.substr(text.find("Korg", 0), 4);
     cout << "_name = " << _name << endl;
   } catch (std::out_of_range &e) {
     cout << "Exception: " << e.what() << endl;
   }
 
   try {
-      string erased = text.erase(text.find("Let"), 10);
-      cout << erased << endl;
+    string erased = text.erase(text.find("Let"), 10);
+    cout << erased << endl;
   } catch (std::out_of_range &e) {
-      cout << "Exception: " << e.what() << endl;
+    cout << "Exception: " << e.what() << endl;
   }
+
+  string _newName = "I am Thor, Son of Odin";
+  text.insert(12, _newName);
+
+  cout << text << endl;
 }
