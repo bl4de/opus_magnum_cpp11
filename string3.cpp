3b3aeb7b6893ef85506c7a51545c7b52030b02aa
bloorq@gmail.com <bloorq@gmail.com>
2018-04-29 23:22:46 +0100

    substr()

---
+++ string3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#include "oposy.cpp"
+
+using namespace std;
+using namespace oposy;
+
+int main() {
+  string text = "Let me introduce myself, my name is Korg";
+  try {
+    string _name = text.substr(12, 4);
+    cout << "_name = " << _name << endl;
+  } catch (std::out_of_range &e) {
+    cout << "Exception: " << e.what() << endl;
+  }
+}
