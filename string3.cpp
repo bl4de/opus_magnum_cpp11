468ae909f0aab48c463d1e374ebef386cf8778a4
bloorq@gmail.com <bloorq@gmail.com>
2018-04-29 23:32:35 +0100

    erase()

--- string3.cpp
+++ string3.cpp
@@ -8,10 +8,18 @@ using namespace oposy;
 
 int main() {
   string text = "Let me introduce myself, my name is Korg";
+  
   try {
     string _name = text.substr(text.find("Korg",0), 4);
     cout << "_name = " << _name << endl;
   } catch (std::out_of_range &e) {
     cout << "Exception: " << e.what() << endl;
+  }
+
+  try {
+      string erased = text.erase(text.find("Let"), 10);
+      cout << erased << endl;
+  } catch (std::out_of_range &e) {
+      cout << "Exception: " << e.what() << endl;
   }
 }
